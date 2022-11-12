40d3136f1828d0cfce61f1198d024d0b34cfc1d7
lestherl12 <106944199+lestherl12@users.noreply.github.com>
2022-11-12 08:43:00 -0600

    Add files via upload

---
+++ Vector (2).py
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaVector1
+{
+    class PruebaVector1
+    {
+        private int[] sueldos;
+
+        public void Cargar()
+        {
+            sueldos = new int[5];
+            for (int f = 0; f < 5; f++)
+            {
+                Console.Write("Ingrese valor de la componente:");
+                String linea;
+                linea = Console.ReadLine();
+                sueldos[f] = int.Parse(linea);
+            }
+        }
+    
+        public void Imprimir() 
+        {
+            for(int f = 0; f < 5; f++) 
+            {
+                Console.WriteLine(sueldos[f]);
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            PruebaVector1 pv = new PruebaVector1();
+            pv.Cargar();
+            pv.Imprimir();
+        }
+    }
+}
